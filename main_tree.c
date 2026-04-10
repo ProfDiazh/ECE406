4ea5446e08e3c3bc5e4637dc577ed9e458fd6ac9
Diana Diazh <ddiana@arizona.edu>
2026-04-10 10:45:43 -0700

    Update main_tree.c

--- main_tree.c
+++ main_tree.c
@@ -3,14 +3,21 @@
 extern struct decl * parser_result;
 extern int yyparse();
 extern void decl_print(struct decl *e );
+extern FILE *yyin;
 
 int main()
 {
-   if(yyparse()==0) {
-      printf("Parse successful!\n AST: \n ------- \n");
-      decl_print(parser_result);
-      printf("\n");
-   } else {
-      printf("Parse failed.\n");
-   }
+	yyin = fopen("program.c","r"); // Set Flex's input stream to your file
+	if(!yyin) {
+		printf("Could not open program.c!\n");
+		return 1;
+	}
+	
+	if(yyparse()==0) { 		// Start parsing
+	  printf("Parse successful!\n AST: \n ------- \n");
+	  decl_print(parser_result);
+	  printf("\n");
+	} else {
+	  printf("Parse failed.\n");
+	}
 }
